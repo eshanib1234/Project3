773692df90336c6ebdd88bd17e8e19c5f7f712c4
eshanib1234 <153485588+eshanib1234@users.noreply.github.com>
2024-02-05 11:38:56 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,6 @@
 string=input(("Enter a letter:"))  
 if(string==string[::-1]):  
-      print("The letter is a palindrome")  
+      print("The letter is a palindrome") 
+      print("URK23CS1157")
 else:  
       print("The letter is not a palindrome")  
